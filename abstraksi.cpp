fae8b40427b35f694a1a02526df51816627a48c7
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2025-06-03 15:15:23 +0700

    Membuat member display utk tampilan

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -13,3 +13,10 @@ class AbstraksiKlas {
             y = b;
         }
 
+        //menampilkan nilai
+        void display() {
+            cout << " x = " << x << endl;
+            cout << " y = " << y << endl;
+        }
+};
+
